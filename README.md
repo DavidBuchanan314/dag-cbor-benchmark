229948f664f32f8223081921454be05773ba41ea
David Buchanan <d@vidbuchanan.co.uk>
2024-02-24 13:01:09 +0000

    python benchmarks

--- README.md
+++ README.md
@@ -9,6 +9,12 @@ data/citm_catalog.json.dagcbor     342_373 bytes  - moderately nested objects, i
 data/twitter.json.dagcbor          402_814 btyes  - ditto
 ```
 
+Trivial test cases:
+
+```
+data/trivial_helloworld.dagcbor     14 bytes  - A hello world string (sanity check and/or test constant overheads)
+```
+
 Synthetic torture tests:
 
 ```
@@ -17,4 +23,63 @@ data/torture_nested_maps.dagcbor   20_000_001 bytes  - 10M nested maps (each wit
 data/torture_cids.dagcbor           4_100_005 btyes  - 100K CIDs (in a flat list)
 ```
 
-TODO: gather some more real-world DAG-CBOR from bluesky, ideally anonymised
+TODO: gather some more real-world DAG-CBOR from bluesky, ideally anonymised (maybe just the MST blocks?)
+
+TODO: include CAR test cases
+
+
+## Python shootout:
+
+Resutlts of `bench.py`, on Python 3.12.1, Fedora 39, 2021 M1 Pro MBP
+
+Contestants:
+
+- [DavidBuchanan314/dag-cbrrr](https://github.com/DavidBuchanan314/dag-cbrrr) (commit 2f4171e9136f022fd5beb288c767591d57e513c6)
+- [MarshalX/python-libipld](https://github.com/MarshalX/python-libipld) (release v1.1.0) (Decode only) (NOTE: There's an improved `fix-perf` branch that I haven't tested yet)
+- [hashberg-io/dag-cbor](https://github.com/hashberg-io/dag-cbor) (release v0.3.3)
+
+```
+Hello World Decode:
+===================
+cbrrr    : 210 ns
+libipld  : 454 ns
+dag_cbor : 4227 ns
+
+Hello World Encode:
+===================
+cbrrr    : 135 ns
+dag_cbor : 5063 ns
+
+Realistic Decode Tests:
+=======================
+canada.json.dagcbor            cbrrr    : 3.73 ms (269.80 MB/s)
+canada.json.dagcbor            libipld  : 26.95 ms (37.38 MB/s)
+canada.json.dagcbor            dag_cbor : 106.04 ms (9.50 MB/s)
+citm_catalog.json.dagcbor      cbrrr    : 2.44 ms (133.77 MB/s)
+citm_catalog.json.dagcbor      libipld  : 17.08 ms (19.12 MB/s)
+citm_catalog.json.dagcbor      dag_cbor : 45.63 ms (7.16 MB/s)
+twitter.json.dagcbor           cbrrr    : 1.35 ms (284.89 MB/s)
+twitter.json.dagcbor           libipld  : 10.83 ms (35.48 MB/s)
+twitter.json.dagcbor           dag_cbor : 18.94 ms (20.28 MB/s)
+
+Realistic Encode Tests:
+=======================
+canada.json.dagcbor            dag_cbor : 0.91 ms (1109.95 MB/s)
+canada.json.dagcbor            dag_cbor : 232.15 ms (4.34 MB/s)
+citm_catalog.json.dagcbor      dag_cbor : 1.38 ms (236.92 MB/s)
+citm_catalog.json.dagcbor      dag_cbor : 64.69 ms (5.05 MB/s)
+twitter.json.dagcbor           dag_cbor : 0.63 ms (605.39 MB/s)
+twitter.json.dagcbor           dag_cbor : 24.99 ms (15.37 MB/s)
+
+Decode Torture Tests:
+=====================
+torture_nested_lists.dagcbor   cbrrr     771.4 ms (12.36 MB/s)
+torture_nested_lists.dagcbor   libipld   SEGFAULT
+torture_nested_lists.dagcbor   dag_cbor  ERROR: maximum recursion depth exceeded
+torture_nested_maps.dagcbor    cbrrr     1274.0 ms (14.97 MB/s)
+torture_nested_maps.dagcbor    libipld   SEGFAULT
+torture_nested_maps.dagcbor    dag_cbor  ERROR: maximum recursion depth exceeded
+torture_cids.dagcbor           cbrrr     40.4 ms (96.67 MB/s)
+torture_cids.dagcbor           libipld   68.5 ms (57.08 MB/s)
+torture_cids.dagcbor           dag_cbor  7399.3 ms (0.53 MB/s)
+```
---
+++ bench.py
@@ -0,0 +1,109 @@
+import os
+import time
+import timeit
+from typing import Dict
+from multiprocessing import Process, Queue
+
+# our contestants:
+import cbrrr     # https://github.com/DavidBuchanan314/dag-cbrrr
+import libipld   # https://github.com/MarshalX/python-libipld
+import dag_cbor  # https://github.com/hashberg-io/dag-cbor
+
+decoders = {
+	"cbrrr": cbrrr.decode_dag_cbor,
+	"libipld": libipld.decode_dag_cbor,
+	"dag_cbor": dag_cbor.decode,
+}
+
+encoders = {
+	"cbrrr": cbrrr.encode_dag_cbor,
+	# libipld does not support encoding
+	"dag_cbor": dag_cbor.encode,
+}
+
+DATA_DIR = "./data/"
+
+testcases: Dict[str, bytes] = {}
+
+for testcase in os.listdir(DATA_DIR):
+	data = open(DATA_DIR + testcase, "rb").read()
+	testcases[testcase] = data
+
+print()
+print("Hello World Decode:")
+print("===================")
+HELLO_ITERS = 100000
+for name, decodefn in decoders.items():
+	res = timeit.timeit(lambda: decodefn(testcases["trivial_helloworld.dagcbor"]), number=HELLO_ITERS)
+	ns_per_it = (res / HELLO_ITERS) * 1000 * 1000 * 1000
+	print(f"{name:<9}: {ns_per_it:.0f} ns")
+
+print()
+print("Hello World Encode:")
+print("===================")
+HELLO_ITERS = 100000
+for name, encodefn in encoders.items():
+	decoded = decoders[name](testcases["trivial_helloworld.dagcbor"])
+	res = timeit.timeit(lambda: encodefn(decoded), number=HELLO_ITERS)
+	ns_per_it = (res / HELLO_ITERS) * 1000 * 1000 * 1000
+	print(f"{name:<9}: {ns_per_it:.0f} ns")
+
+print()
+print("Realistic Decode Tests:")
+print("=======================")
+REALISTIC_ITERS = 25
+for testname, testcase in testcases.items():
+	if testname.startswith("torture_") or testname.startswith("trivial_"):
+		continue
+	for decodername, decodefn in decoders.items():
+		res = timeit.timeit(lambda: decodefn(testcase), number=REALISTIC_ITERS)
+		res /= REALISTIC_ITERS
+		ms_per_it = res * 1000
+		mbps = len(testcase)/1024/1024/res
+		print(f"{testname:<30} {decodername:<9}: {ms_per_it:.2f} ms ({mbps:.2f} MB/s)")
+
+print()
+print("Realistic Encode Tests:")
+print("=======================")
+REALISTIC_ITERS = 25
+for testname, testcase in testcases.items():
+	if testname.startswith("torture_") or testname.startswith("trivial_"):
+		continue
+	for encodername, encodefn in encoders.items():
+		decoded = decoders[encodername](testcase)
+		res = timeit.timeit(lambda: encodefn(decoded), number=REALISTIC_ITERS)
+		res /= REALISTIC_ITERS
+		ms_per_it = res * 1000
+		mbps = len(testcase)/1024/1024/res
+		print(f"{testname:<30} {decodername:<9}: {ms_per_it:.2f} ms ({mbps:.2f} MB/s)")
+
+print()
+print("Decode Torture Tests:")
+print("=====================")
+for testname, testcase in testcases.items():
+	if not testname.startswith("torture_"):
+		continue
+	for decodername, decodefn in decoders.items():
+		q = Queue()
+		def run_and_except():
+			try:
+				decodefn(testcase)
+				q.put("success")
+			except Exception as e:
+				q.put(str(e))
+		p = Process(target=run_and_except)
+		start = time.time()
+		p.start()
+		p.join()
+		duration = time.time() - start
+		if p.exitcode:
+			print(testname.ljust(30, " "), decodername.ljust(9, " "), "SEGFAULT" if p.exitcode == -11 else "ERROR")
+			continue
+
+		res = q.get()
+		if res == "success":
+			duration_ms = duration * 1000
+			mbps = len(testcase)/1024/1024/duration
+			print(testname.ljust(30, " "), decodername.ljust(9, " "), f"{duration_ms:.1f} ms ({mbps:.2f} MB/s)")
+		else:
+			print(testname.ljust(30, " "), decodername.ljust(9, " "), "ERROR:", res)
